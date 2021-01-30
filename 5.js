dbee88d3a49d84a2f14a4ec2b08b86d91fcbf24c
Praveen Kumar <praveenscience@gmail.com>
2021-01-30 16:47:08 +0000

    Boolean Comparisons!

---
+++ 5.js
@@ -0,0 +1,9 @@
+// 8 types of comparisons.
+/* false */ console.log(5 < 5);
+/* false */ console.log(5 > 5);
+/* true  */ console.log(5 <= 5);
+/* true  */ console.log(5 >= 5);
+/* true  */ console.log(5 == "5");
+/* false */ console.log(5 === "5");
+/* false */ console.log(5 != "5");
+/* true  */ console.log(5 !== "5");
